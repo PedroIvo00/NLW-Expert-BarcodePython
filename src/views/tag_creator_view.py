f29b1645d629dcafa2c5fed390fbf2dfa70cab6b
PedroIvo00 <pedroivo00@hotmail.com>
2024-02-07 22:10:58 -0300

    feat: Implementing controllers for barcode generation

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,17 +1,19 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
         responsability for interacting with HTTP
     '''
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
         body = http_request.body
         product_code = body["product_code"]
         # lógica de regra de negócio
-        print('estou na minha view"'+product_code)
+        formatted_response = tag_creator_controller.create(product_code)
         # retorno HTTP
         return HttpResponse(
             status_code=200,
-            body={"hello": "world"}
+            body=formatted_response
             )
